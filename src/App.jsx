fcc4f4662f8a36f3461ea6af09b85f1a63301a10
anthonyjuan <anthony.christian@everywhere.id>
2024-03-19 12:22:00 +0700

    add download handler

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { createSignal, Show, Index } from "solid-js";
-import imglyRemoveBackground from "@imgly/background-removal"
+import imglyRemoveBackground from "@imgly/background-removal";
+import { toJpeg } from "html-to-image";
 
 import styles from "./App.module.css";
 
 function App() {
   const [selectedImage, setSelectedImage] = createSignal("");
+  const [onLoading, setOnLoading] = createSignal(false);
 
   return (
     <div class={styles.App}>
@@ -12,7 +14,7 @@ function App() {
         <div class="wrapper bg-white rounded-lg px-6 py-8 ring-1 ring-slate-900/5 shadow-xl">
           <div class={styles.titleGradient}>pasputu</div>
           <p class="text-slate-600 mt-8 text-sm tracking-wide">
-            simple app to replace your background using your camera/photos, and
+            simple app to replace your background using your photos or camera(on development), and
             change with your desired background
           </p>
           <div>
@@ -20,38 +22,61 @@ function App() {
               type="file"
               id="fileInput"
               accept="image/*"
-              onChange={async (e) =>{
+              onChange={async (e) => {
                 const file = e.target.files[0];
-                console.log("loading")
-                const imgWithoutBg = await imglyRemoveBackground(file)
-                setSelectedImage(URL.createObjectURL(imgWithoutBg));
-                console.log("finished")
+                setOnLoading(true);
+                const imgWithoutBg = await imglyRemoveBackground(file);
+                const reader = new FileReader();
+                reader.onload = () => {
+                    setSelectedImage(reader.result);
+                };
+                reader.readAsDataURL(imgWithoutBg);
+                setOnLoading(false);
               }}
               style={{ display: "none" }} // Hide the file input
             />
             <div class="mt-8 flex items-center justify-center gap-x-6">
-              <button class="rounded-md bg-cyan-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-cyan-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-                Take photo
-              </button>
-              <p class="text-slate-500"> or </p>
-              <button
-                onClick={() => {
-                  document.getElementById("fileInput").click();
-                }}
-                class="rounded-md bg-cyan-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-cyan-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              {/* <Show
+                when={!onLoading()}
+                fallback={
+                  <button
+                    class="bg-cyan-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm rounded-md cursor-not-allowed opacity-70"
+                    disabled
+                  >
+                    Processing...
+                  </button>
+                }
               >
-                Upload image
-              </button>
+                <button class="rounded-md bg-cyan-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-cyan-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                  Take photo
+                </button>
+              </Show>
+
+              <p class="text-slate-500"> or </p> */}
+
+              <Show
+                when={!onLoading()}
+                fallback={
+                  <button
+                    class="bg-cyan-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm rounded-md cursor-not-allowed opacity-70"
+                    disabled
+                  >
+                    Processing...
+                  </button>
+                }
+              >
+                <button
+                  onClick={() => {
+                    document.getElementById("fileInput").click();
+                  }}
+                  class="rounded-md bg-cyan-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-cyan-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                >
+                  Upload image
+                </button>
+              </Show>
             </div>
           </div>
-          <Editor image={selectedImage()} />
-          <div>
-            <div class="mt-8 flex items-center justify-center gap-x-6">
-              <button class="rounded-md bg-cyan-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-cyan-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-                Download
-              </button>
-            </div>
-          </div>
+          <Editor image={selectedImage()} fileName={fileName()} onLoading={onLoading} />
         </div>
       </header>
     </div>
@@ -70,40 +95,75 @@ function Editor(props) {
   const onRatioChange = (e) => {
     const ratio = e.target.value;
     if (ratio == "1:1") {
-      setWidth("300")
-      setHeight("300")
+      setWidth("300");
+      setHeight("300");
     } else if (ratio == "3:4") {
-      setWidth("225")
-      setHeight("300")
+      setWidth("225");
+      setHeight("300");
     } else if (ratio == "2:3") {
-      setWidth("200")
-      setHeight("300")
+      setWidth("200");
+      setHeight("300");
     }
   };
 
-  const onBgChange = (e) => {
-    setBgColor(e.target.value);
-  };
+  const onDownload = () => {
+    if (imgRef === null) {
+      return
+    }
+
+    toJpeg(imgRef, { cacheBust: true }).then((dataUrl) => {
+      const link = document.createElement("a")
+      link.download = `pasputu.jpeg`
+      link.href = dataUrl
+      link.click()
+    }).catch((error) => {
+      console.error("Error:", error)
+    })
+  }
 
   return (
     <div class="flex flex-col justify-center items-center bg-stone-100 p-8 mt-8 rounded-lg">
       <div
         ref={imgRef}
-        class={`mb-8 bg-${bgColor()}-600 rounded-lg ring-1 ring-neutral-400/5 shadow-xl overflow-hidden relative top-0 left-0`}
+        class={`mb-8 ring-1 ring-neutral-400/5 shadow-xl overflow-hidden relative top-0 left-0`}
         style={{
           width: `${width()}px`,
           height: `${height()}px`,
+          "background-color": bgColor(),
         }}
       >
-        <Show when={props.image !== null} fallback={<p>no image</p>}>
+        <Show
+          when={props.image !== ""}
+          fallback={
+            <div
+              class={`${
+                props.onLoading() && "animate-pulse"
+              } w-full h-full flex justify-center items-center flex-col rounded`}
+              style={{
+                "background-color": bgColor(),
+              }}
+            >
+              <svg
+                class="w-10 h-10 text-gray-600"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+                viewBox="0 0 20 18"
+              >
+                <path d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z" />
+              </svg>
+            </div>
+          }
+        >
           <div
-            className={`relative overflow-hidden bg-no-repeat`}
+            className={`relative overflow-hidden}`}
             style={{
               width: `${width()}px`,
               height: `${height()}px`,
+              "background-repeat": "no-repeat",
               "background-image": `url(${props.image})`,
-              "background-position-x": `${getVert()}px`,
-              "background-position-y": `${getHoriz()}px`,
+              "background-position-x": `${getHoriz()}px`,
+              "background-position-y": `${getVert()}px`,
               "background-size": `${scale() * 100}%`,
             }}
           ></div>
@@ -119,7 +179,7 @@ function Editor(props) {
           </label>
           <DropdownSelector
             opts={["white", "blue", "red"]}
-            onDataChange={onBgChange}
+            onDataChange={(e) => setBgColor(e.target.value)}
           />
         </div>
         <div class="px-8 text-start">
@@ -185,6 +245,14 @@ function Editor(props) {
           onInput={(e) => setScale(e.target.value)}
           class="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700"
         ></input>
+      </div>
+
+      <div>
+        <div class="mt-8 flex items-center justify-center gap-x-6">
+          <button onClick={onDownload} class="rounded-md bg-cyan-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-cyan-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+            Download
+          </button>
+        </div>
       </div>
     </div>
   );
