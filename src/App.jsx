b6bfc0b5aa3686c5669962a1acb8aa04193cd948
anthonyjuan <anthony.christian@everywhere.id>
2024-03-19 17:00:50 +0700

    adjust flow loading

--- src/App.jsx
+++ src/App.jsx
@@ -174,7 +174,7 @@ function Editor(props) {
         }}
       >
         <Show
-          when={props.image !== ""}
+          when={props.image !== "" && !props.processImage && !props.onLoading}
           fallback={
             <div
               class={`w-full h-full flex justify-center items-center flex-col rounded px-2`}
@@ -206,7 +206,6 @@ function Editor(props) {
                         class="bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full"
                         style={{ width: `${props.removalProgress}%` }}
                       >
-                        {" "}
                         {props.removalProgress}%
                       </div>
                     </div>
@@ -215,7 +214,7 @@ function Editor(props) {
                   <div role="status">
                     <svg
                       aria-hidden="true"
-                      class="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+                      class="w-8 h-8 text-gray-200 animate-spin fill-blue-600"
                       viewBox="0 0 100 101"
                       fill="none"
                       xmlns="http://www.w3.org/2000/svg"
